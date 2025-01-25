26ac8829e52c436eb1c97c3a2e0cb7ce6de88bb3
ElectriPixie <ElectriPixie@gmail.com>
2025-01-25 14:34:18 -0500

    updated main.tf to pas service_endpoints

--- main.tf
+++ main.tf
@@ -17,38 +17,51 @@ module localstack_network {
   providers = { docker=docker }
 }
 
+module "localstack" {
+  source = "./modules/localstack"
+  providers = { docker=docker }
+  network_name = module.localstack_network.network_name
+  service_endpoints = var.service_endpoints
+}
+
 module "localstack_s3" {
   source = "./modules/localstack_s3"
   providers = { docker=docker }
   network_name = module.localstack_network.network_name
+  service_endpoints = var.service_endpoints
 }
 
 module "localstack_dynamodb" {
   source = "./modules/localstack_dynamodb"
   providers = { docker=docker }
   network_name = module.localstack_network.network_name
+  service_endpoints = var.service_endpoints
 }
 
 module "localstack_lambda" {
   source = "./modules/localstack_lambda"
   providers = { docker=docker }
   network_name = module.localstack_network.network_name
+  service_endpoints = var.service_endpoints
 }
 
 module "localstack_api_gateway" {
   source = "./modules/localstack_api_gateway"
   providers = { docker=docker }
   network_name = module.localstack_network.network_name
+  service_endpoints = var.service_endpoints
 }
 
 module "localstack_cloudwatch" {
   source = "./modules/localstack_cloudwatch"
   providers = { docker=docker }
   network_name = module.localstack_network.network_name
+  service_endpoints = var.service_endpoints
 }
 
 module "localstack_ec2" {
   source = "./modules/localstack_ec2"
   providers = { docker=docker }
   network_name = module.localstack_network.network_name
+  service_endpoints = var.service_endpoints
 }
