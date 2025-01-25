c9efc6407398bd28bc4bab421f19cf872485e6fe
ElectriPixie <ElectriPixie@gmail.com>
2025-01-25 13:33:26 -0500

    fixed type

--- modules/localstack_cloudwatch/variables.tf
+++ modules/localstack_cloudwatch/variables.tf
@@ -1,12 +1,12 @@
 variable "network_name" {}
 variable "service_endpoints" {
   description = "Map of environment variables for configuring LocalStack service endpoints."
-  type = list(string)
+  type = map(string)
   default = {}
 }
 variable "environment" {
   description = "A map of environment variables for LocalStack services."
-  type = list(string)
+  type = map(string)
   default = {
     SERVICES = "cloudwatch"
   }
