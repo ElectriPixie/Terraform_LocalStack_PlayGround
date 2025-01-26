695b007638a0fe82610004d2ff93dd6f7e2da0a8
ElectriPixie <ElectriPixie@gmail.com>
2025-01-26 17:26:28 -0500

    updated main.tf

--- playground/main.tf
+++ playground/main.tf
@@ -1,12 +1,12 @@
 # File: main.tf
 
 module localstack_network {
-  source = "https://github.com/ElectriPixie/Terraform_LocalStack_Network_Module.git"
+  source = "git::https://github.com/ElectriPixie/Terraform_LocalStack_Network_Module.git"
   providers = { docker=docker }
 }
 
 module "localstack" {
-  source = ""
+  source = "git::https://github.com/ElectriPixie/Terraform_LocalStack_Module.git"
   providers = { docker=docker }
   network_name = module.localstack_network.network_name
   environment_root = var.environment_root
