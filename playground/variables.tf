d732b5ecd36cc1b3e84d819f763124946ed35128
ElectriPixie <ElectriPixie@gmail.com>
2025-01-27 19:26:53 -0500

    updated variables

--- playground/variables.tf
+++ playground/variables.tf
@@ -3,8 +3,8 @@ variable "environment_root" {
   type = map(string)
   default = {
     DEFAULT_REGION          = "us-east-1"
-    AWS_ACCESS_KEY_ID       = "test"     # Set your mock access key (LocalStack default)
-    AWS_SECRET_ACCESS_KEY   = "test"     # Set your mock secret key (LocalStack default)
+    AWS_ACCESS_KEY_ID       = "my_secret_key"     # Set your mock access key (LocalStack default)
+    AWS_SECRET_ACCESS_KEY   = "my_secret_key"     # Set your mock secret key (LocalStack default)
     AWS_DEFAULT_REGION      = "us-east-1"
   }
 }
